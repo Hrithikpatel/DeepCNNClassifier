8e57606713e34b1a3b73e51765742617604d358c
Hrithikpatel <hrithikpatel200@gmail.com>
2024-05-06 11:08:36 +0530

    Data Ingestion components and config modified

--- src/DeepClassifier/config/__init__.py
+++ src/DeepClassifier/config/__init__.py
@@ -16,14 +16,15 @@ class ConfigurationManager:
 
 
     def get_data_ingestion_config(self)->DataIngestionConfig:
+        config = self.config.data_ingestion
         
-        create_directories(self.config.root_dir)
+        create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
-            root_dir = self.config.root_dir,
-            source_URL = self.config.source_URL,
-            local_data_file = self.config.local_data_file,
-            unzip_dir = self.config.unzip_dir
+            root_dir = config.root_dir,
+            source_URL = config.source_URL,
+            local_data_file = config.local_data_file,
+            unzip_dir = config.unzip_dir
         )
 
         return data_ingestion_config
