a809b6675376c76dff1561d9d0b1e266b2b36b35
Hrithikpatel <hrithikpatel200@gmail.com>
2024-05-06 17:20:53 +0530

    Added entity for prepare base model

--- src/DeepClassifier/config/__init__.py
+++ src/DeepClassifier/config/__init__.py
@@ -1,9 +1,11 @@
 import sys
 import os
+from pathlib import Path
 from DeepClassifier.constant import CONFIG_FILE_PATH,PARAMS_FILE_PATH,PROJECT_ROOT_DIR
-from DeepClassifier.utils import *
-from DeepClassifier.entity import DataIngestionConfig
+from DeepClassifier.utils import  read_yaml, create_directories
+from DeepClassifier.entity import (DataIngestionConfig,PrepareBaseModelConfig)
 sys.path.append(PROJECT_ROOT_DIR)
+
 
 class ConfigurationManager:
 
@@ -21,12 +23,29 @@ class ConfigurationManager:
         create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
-            root_dir = config.root_dir,
+            root_dir = Path(config.root_dir),
             source_URL = config.source_URL,
-            local_data_file = config.local_data_file,
-            unzip_dir = config.unzip_dir
+            local_data_file = Path(config.local_data_file),
+            unzip_dir = Path(config.unzip_dir)
         )
 
         return data_ingestion_config
     
     
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
--- src/DeepClassifier/entity/config_entity.py
+++ src/DeepClassifier/entity/config_entity.py
@@ -1,14 +1,22 @@
-import sys
-from collections import namedtuple
-from DeepClassifier.constant import PROJECT_ROOT_DIR
-sys.path.append(PROJECT_ROOT_DIR)
+from dataclasses import dataclass
+from pathlib import Path
 
 
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
 
-DataIngestionConfig = namedtuple("DataIngestionConfig",[
-    "root_dir",
-    "source_URL",
-    "local_data_file",
-    "unzip_dir"
-])
 
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
